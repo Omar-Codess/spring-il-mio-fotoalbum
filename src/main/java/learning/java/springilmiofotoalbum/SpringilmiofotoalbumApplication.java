d9624b1ef6f83a7fa2969bc170fce6976c7e18e1
Omar-Codess <omartaki8@gmail.com>
2023-04-13 12:40:01 +0200

    Initial commit

---
+++ src/main/java/learning/java/springilmiofotoalbum/SpringilmiofotoalbumApplication.java
@@ -0,0 +1,13 @@
+package learning.java.springilmiofotoalbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringilmiofotoalbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringilmiofotoalbumApplication.class, args);
+	}
+
+}
